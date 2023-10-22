b756f4d81f52182ce8c60cb2ce3eaa6be0547587
nevadskiy <nevadskiy@gmail.com>
2023-10-22 21:13:34 +0300

    feature `set` method

--- src/ManyToAnyRelationStrategy.php
+++ src/ManyToAnyRelationStrategy.php
@@ -52,8 +52,66 @@ class ManyToAnyRelationStrategy implements Strategy
 
     public function set(NovaRequest $request, $requestAttribute, $model, $attribute): callable
     {
-        return function () use ($request, $requestAttribute, $model) {
-            // @todo
+        return function () use ($request, $requestAttribute, $model, $attribute) {
+            $resourcesByMorphClass = [];
+
+            foreach ($this->field->resources as $resource) {
+                $resourcesByMorphClass[$resource::newModel()->getMorphClass()] = $resource;
+            }
+
+            $collectionModels = $model->{$attribute}()->get();
+
+            $collectionModelsDictionary = [];
+
+            foreach ($collectionModels as $collectionModel) {
+                $collectionModelsDictionary[$collectionModel->getKey().':'.$resourcesByMorphClass[$collectionModel->getMorphClass()]::uriKey()] = $collectionModel;
+            }
+
+            $requestModels = $request->all()[$requestAttribute] ?? [];
+
+            $requestModelsDictionary = [];
+
+            foreach ($requestModels as $requestModel) {
+                $requestModelsDictionary[$requestModel['id'].':'.$requestModel['type']] = $requestModel;
+            }
+
+            $collectionModelsForDetach = [];
+
+            foreach ($collectionModelsDictionary as $collectionKey => $collectionModel) {
+                if (! isset($requestModelsDictionary[$collectionKey])) {
+                    $collectionModelsForDetach[] = $collectionModel;
+                }
+            }
+
+            foreach ($collectionModelsForDetach as $collectionModelForDetach) {
+                $model->{$attribute}()->detach($collectionModelForDetach);
+            }
+
+            $resourcesByType = [];
+
+            foreach ($this->field->resources as $resource) {
+                $resourcesByType[$resource::uriKey()] = $resource;
+            }
+
+            foreach ($requestModels as $index => $requestModel) {
+                if ($requestModel['mode'] === 'create') {
+                    $collectionModelForCreate = $this->createResourceModel($resourcesByType[$requestModel['type']], $requestModel['attributes']);
+
+                    $model->{$attribute}()->attach($collectionModelForCreate, ['position' => $index]);
+                } else if ($requestModel['mode'] === 'attach') {
+                    $collectionModelForAttach = $resourcesByType[$requestModel['type']]::newModel()->newQuery()->findOrFail($requestModel['id']);
+
+                    $this->updateResourceModel($collectionModelForAttach, $resourcesByType[$requestModel['type']], $requestModel['attributes']);
+
+                    $model->{$attribute}()->attach($collectionModelForAttach, ['position' => $index]);
+                } else if ($requestModel['mode'] === 'update') {
+                    $collectionModelForUpdate = $collectionModelsDictionary[$requestModel['id'].':'.$requestModel['type']];
+
+                    $this->updateResourceModel($collectionModelForUpdate, $resourcesByType[$requestModel['type']], $requestModel['attributes']);
+
+                    $model->{$attribute}()->update($collectionModelForUpdate, ['position' => $index]);
+                }
+            }
         };
     }
 
