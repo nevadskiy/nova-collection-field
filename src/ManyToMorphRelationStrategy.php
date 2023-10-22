3666c0804eaf3c394293b1c1cd80ae99bd8a950e
nevadskiy <nevadskiy@gmail.com>
2023-10-22 23:58:17 +0300

    refactor

--- src/ManyToMorphRelationStrategy.php
+++ src/ManyToMorphRelationStrategy.php
@@ -3,12 +3,13 @@
 namespace Nevadskiy\Nova\Collection;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\UploadedFile;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Resource;
 
 class ManyToMorphRelationStrategy implements Strategy
 {
+    use InteractsWithResourceFields;
+
     protected ManyToMorphCollection $field;
 
     public function setField(ManyToMorphCollection $field): void
@@ -137,50 +138,6 @@ class ManyToMorphRelationStrategy implements Strategy
         }
 
         return $collectionForDetach;
-    }
-
-    protected function createResourceModel(string $resourceClass, array $attributes): Model
-    {
-        [$model, $callbacks] = $resourceClass::fill($this->newRequestFromAttributes($attributes), $resourceClass::newModel());
-
-        $model->save();
-
-        foreach ($callbacks as $callback) {
-            $callback();
-        }
-
-        return $model;
-    }
-
-    protected function updateResourceModel(Model $model, string $resourceClass, array $attributes): void
-    {
-        [$model, $callbacks] = $resourceClass::fillForUpdate($this->newRequestFromAttributes($attributes), $model);
-
-        $model->save();
-
-        foreach ($callbacks as $callback) {
-            $callback();
-        }
-    }
-
-    protected function newRequestFromAttributes(array $attributes): NovaRequest
-    {
-        $requestServer = [];
-        $requestFiles = [];
-
-        foreach ($attributes as $attribute => $value) {
-            if ($value instanceof UploadedFile) {
-                $requestFiles[$attribute] = $value;
-            } else {
-                $requestServer[$attribute] = $value;
-            }
-        }
-
-        $request = new NovaRequest([], $requestServer, [], [], $requestFiles);
-
-        $request->setMethod('POST');
-
-        return $request;
     }
 
     protected function getPivotAttributes(int $index): array
--- src/MorphToManyRelationStrategy.php
+++ src/MorphToManyRelationStrategy.php
@@ -3,12 +3,13 @@
 namespace Nevadskiy\Nova\Collection;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\UploadedFile;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Resource;
 
 class MorphToManyRelationStrategy implements Strategy
 {
+    use InteractsWithResourceFields;
+
     protected MorphToManyCollection $field;
 
     public function setField(MorphToManyCollection $field): void
@@ -83,50 +84,6 @@ class MorphToManyRelationStrategy implements Strategy
                 $model->{$relation}()->sync($syncPayload);
             }
         };
-    }
-
-    protected function createResourceModel(string $resourceClass, array $attributes): Model
-    {
-        [$model, $callbacks] = $resourceClass::fill($this->newRequestFromAttributes($attributes), $resourceClass::newModel());
-
-        $model->save();
-
-        foreach ($callbacks as $callback) {
-            $callback();
-        }
-
-        return $model;
-    }
-
-    protected function updateResourceModel(Model $model, string $resourceClass, array $attributes): void
-    {
-        [$model, $callbacks] = $resourceClass::fillForUpdate($this->newRequestFromAttributes($attributes), $model);
-
-        $model->save();
-
-        foreach ($callbacks as $callback) {
-            $callback();
-        }
-    }
-
-    protected function newRequestFromAttributes(array $attributes): NovaRequest
-    {
-        $requestServer = [];
-        $requestFiles = [];
-
-        foreach ($attributes as $attribute => $value) {
-            if ($value instanceof UploadedFile) {
-                $requestFiles[$attribute] = $value;
-            } else {
-                $requestServer[$attribute] = $value;
-            }
-        }
-
-        $request = new NovaRequest([], $requestServer, [], [], $requestFiles);
-
-        $request->setMethod('POST');
-
-        return $request;
     }
 
     protected function getPivotAttributes(array $resource): array
